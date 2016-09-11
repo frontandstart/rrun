bc4e591cf9fec7daa64f2a527c5bab2869822e11
Rrr <mail@piranis.ru>
2016-09-11 03:42:54 +0300

    rvm first than oh-my-zsh

--- rrun.sh
+++ rrun.sh
@@ -9,10 +9,14 @@ printf "|-----------------------------------|\n"
 printf "|-- install min software          --|\n"
 sudo apt-get -y install nano curl wget zsh git libgmp-dev nginx &&\
 printf "|-----------------------------------|\n"
+printf "|-- install rvm & stable ruby     --|\n"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 &&\
+curl -sSL https://get.rvm.io | bash -s stable --ruby &&\
+source /home/$(id -u -n)/.rvm/scripts/rvm &&\
+gem install rails \
+printf "|-----------------------------------|\n"
 printf "|-- install oh-my-zsh             --|\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&\
 printf "|-----------------------------------|\n"
-printf "|-- install rvm & stable ruby     --|\n"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && curl -sSL https://get.rvm.io | bash -s stable --ruby && source /home/$(id -u -n)/.rvm/scripts/rvm && gem install rails \
 printf "|--------------end------------------|\n"
   
