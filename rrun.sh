1c5ab750e0861457b45b0fbca868c7840cc9712f
Rrr <mail@piranis.ru>
2016-09-11 05:27:36 +0300

    add lines

--- rrun.sh
+++ rrun.sh
@@ -1,15 +1,18 @@
 #! /bin/bash 
 printf "|---------------start---------------|\n" &&\
 printf "|-----set current user password-----|\n" &&\
+printf "|-----------------------------------|\n" &&\
 sudo passwd $(id -u -n) &&\
 printf "|-----------------------------------|\n" &&\
 printf "|---------update & uppgrade---------|\n" &&\
+printf "|-----------------------------------|\n" &&\
 sudo apt-get -y update && sudo apt-get -y upgrade &&\
 printf "|-----------------------------------|\n" &&\
 printf "|--------install min software-------|\n" &&\
 sudo apt-get -y install nano curl wget zsh git libgmp-dev nginx &&\
 printf "|-----------------------------------|\n" &&\
 printf "|----install rvm & stable ruby------|\n" &&\
+printf "|-----------------------------------|\n" &&\
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 &&\
 \curl -sSL https://get.rvm.io | bash -s stable --ruby &&\
 source /home/$(id -u -n)/.rvm/scripts/rvm &&\
