700a81629b42250de77ba513f42ff531c96fce13
Rrr <mail@piranis.ru>
2016-09-11 03:39:10 +0300

    fix rvm installation

--- rrun.sh
+++ rrun.sh
@@ -13,7 +13,6 @@ printf "|-- install oh-my-zsh             --|\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&\
 printf "|-----------------------------------|\n"
 printf "|-- install rvm & stable ruby     --|\n"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \curl -sSL https://get.rvm.io | bash -s stable --ruby && source /home/$(id -u -n)/.rvm/scripts/rvm \
-gem install rails \
-printf "|-----------------end---------------|\n"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && curl -sSL https://get.rvm.io | bash -s stable --ruby && source /home/$(id -u -n)/.rvm/scripts/rvm && gem install rails \
+printf "|--------------end------------------|\n"
   
