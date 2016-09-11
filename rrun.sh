15612dcfa3fe24ec3d28b1b67623e0ac76db2bba
Rrr <mail@piranis.ru>
2016-09-11 03:29:05 +0300

    linebreaks add

--- rrun.sh
+++ rrun.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
-printf "|---------------start---------------|"
-printf "|-- set current user password     --|"
+printf "|---------------start---------------|"\
+printf "|-- set current user password     --|"\
 sudo passwd $(id -u -n) &&\
-printf "|-----------------------------------|"
-printf "|-- update & uppgrade             --|"
+printf "|-----------------------------------|"\
+printf "|-- update & uppgrade             --|"\
 sudo apt-get -y update && sudo apt-get -y update &&\
-printf "|-----------------------------------|"
-printf "|-- install min software          --|"
+printf "|-----------------------------------|"\
+printf "|-- install min software          --|"\
 sudo apt-get -y install curl wget zsh git &&\
-printf "|-----------------------------------|"
-printf "|-- install oh-my-zsh             --|"
+printf "|-----------------------------------|"\
+printf "|-- install oh-my-zsh             --|"\
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&\
-printf "|-----------------------------------|"
-printf "|-- install rvm & stable ruby     --|"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \curl -sSL https://get.rvm.io | bash -s stable --ruby && source /home/$(id -u -n)/.rvm/scripts/rvm
-printf "|-----------------end---------------|"
+printf "|-----------------------------------|"\
+printf "|-- install rvm & stable ruby     --|"\
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \curl -sSL https://get.rvm.io | bash -s stable --ruby && source /home/$(id -u -n)/.rvm/scripts/rvm \
+printf "|-----------------end---------------|"\
