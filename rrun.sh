9c735929ca8bddae5d8179b4c7222ce293bc25f2
Rrr <mail@piranis.ru>
2016-09-11 03:34:30 +0300

    linebreaks add

--- rrun.sh
+++ rrun.sh
@@ -1,17 +1,19 @@
 #!/bin/bash 
-printf "|---------------start---------------|"\
-printf "|-- set current user password     --|"\
+printf "|---------------start---------------|\n"
+printf "|-- set current user password     --|\n"
 sudo passwd $(id -u -n) &&\
-printf "|-----------------------------------|"\
-printf "|-- update & uppgrade             --|"\
+printf "|-----------------------------------|\n"
+printf "|-- update & uppgrade             --|\n"
 sudo apt-get -y update && sudo apt-get -y update &&\
-printf "|-----------------------------------|"\
-printf "|-- install min software          --|"\
-sudo apt-get -y install curl wget zsh git &&\
-printf "|-----------------------------------|"\
-printf "|-- install oh-my-zsh             --|"\
+printf "|-----------------------------------|\n"
+printf "|-- install min software          --|\n"
+sudo apt-get -y install nano curl wget zsh git libgmp-dev nginx &&\
+printf "|-----------------------------------|\n"
+printf "|-- install oh-my-zsh             --|\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&\
-printf "|-----------------------------------|"\
-printf "|-- install rvm & stable ruby     --|"\
+printf "|-----------------------------------|\n"
+printf "|-- install rvm & stable ruby     --|\n"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \curl -sSL https://get.rvm.io | bash -s stable --ruby && source /home/$(id -u -n)/.rvm/scripts/rvm \
-printf "|-----------------end---------------|"\
+gem install rails \
+printf "|-----------------end---------------|\n"
+  
