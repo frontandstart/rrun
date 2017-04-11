17f41061dff8d463963a69be8db9081d6d0bbad9
Rrrcha <mail@piranis.ru>
2017-04-11 21:17:53 +0300

    add nodejs

--- rrun.sh
+++ rrun.sh
@@ -19,14 +19,15 @@ sudo apt-get -y update && sudo apt-get -y upgrade &&\
 printf "|-----------------------------------|\n" &&\
 printf "|--------install min software-------|\n" &&\
 printf "|-----------------------------------|\n" &&\
-sudo apt-get -y install nano curl wget zsh git libgmp-dev nginx &&\
+sudo apt-get -y install nano curl wget zsh git libgmp-dev nginx nodejs &&\
 printf "|-----------------------------------|\n" &&\
 printf "|----install rvm & stable ruby------|\n" &&\
 printf "|-----------------------------------|\n" &&\
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 &&\
 \curl -sSL https://get.rvm.io | bash -s stable --ruby &&\
-source /home/$(id -u -n)/.rvm/scripts/rvm &&\
-gem install rails
+sh -c "$(source /home/$(whoami)/.rvm/scripts/rvm)" &&\
+gem install rails &&\
+gem install bunlder &&\
 printf "|-----------------------------------|\n" &&\
 printf "|--------install oh-my-zsh----------|\n" &&\
 printf "|-----------------------------------|\n" &&\
